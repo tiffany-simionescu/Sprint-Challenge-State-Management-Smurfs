74020b336c92d13b062b12d942efa80748cc05c9
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2019-11-22 21:27:44 -0500

    Added action creators

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,1 +1,33 @@
-// getSmurfInfo add here
+import axios from 'axios';
+
+export const FETCHING_SMURFS_START = 'FETCHING_SMURFS_START';
+export const FETCHING_SMURFS_SUCCESS = 'FETCHING_SMURFS_SUCCESS';
+export const FETCHING_SMURFS_FAILURE = 'FETCHING_SMURFS_FAILURE';
+
+export const ADDING_SMURF_SUCCESS = 'ADDING_SMURF_SUCCESS';
+export const ADDING_SMURF_FAILURE = 'ADDING_SMURF_FAILURE';
+
+export const getSmurfInfo = () => {
+  dispatch({ type: FETCHING_SMURFS_START });
+  axios
+    .get('http://localhost:3333/smurfs')
+    .then(res => {
+      console.log(res);
+      dispatch({ type: FETCHING_SMURFS_SUCCESS, payload: res })
+    })
+    .catch(err => {
+      console.log(err);
+      dispatch({ type: FETCHING_SMURFS_FAILURE, payload: err })
+    })
+}
+
+export const addSmurfInfo = smurf => dispatch => {
+  axios
+    .get('http://localhost:3333/smurfs', smurf)
+    .then(res => {
+      dispatch({ type: ADDING_SMURF_SUCCESS, payload: res })
+    })
+    .catch(err => {
+      dispatch({ type: ADDING_SMURF_FAILURE, payload: err })
+    })
+}
